8dffffbb8b16b141d4169dda9486b3918c47a05b
Andrii Zadorozhnii <andrii.zadorozhnii@infotech.gov.ua>
2026-04-08 16:27:02 +0300

    missed export

--- src/lib.rs
+++ src/lib.rs
@@ -119,7 +119,7 @@ pub use crate::{
     },
     db_iterator::{
         DBIterator, DBIteratorWithThreadMode, DBRawIterator, DBRawIteratorWithThreadMode,
-        DBWALIterator, Direction, IteratorMode,
+        DBWALIterator, Direction, IteratorMode, DBATGIterator, DBATGIteratorWithThreadMode,
     },
     db_options::{
         BlockBasedIndexType, BlockBasedOptions, BlockBasedTablePinningTier,
